078ab1c5d84a9b3f81fae6b3b7b4a41d26724cba
ahmet <ahmet@DESKTOP-CB6V4KQ>
2024-05-24 17:21:44 +0300

    PollicyType Modeli eklendi

--- GarajYeri.Models/Policy.cs
+++ GarajYeri.Models/Policy.cs
@@ -11,5 +11,7 @@ namespace GarajYeri.Models
         public string Name { get; set; }
         public string CompanyName { get; set; }//sigorta firması
         public DateTime Validity { get; set; }// geçerlilik tarihi
+        public int PolicyTypeId { get; set; }
+        public virtual PolicyType PolicyType { get; set; }
     }
 }
