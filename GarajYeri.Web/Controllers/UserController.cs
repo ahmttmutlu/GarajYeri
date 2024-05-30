4505f7df0a564c2c50754f6336cd3e49a5d1f9f8
ahmet <ahmet@DESKTOP-CB6V4KQ>
2024-05-30 19:09:05 +0300

    Diğer düzenlemeler yapıldı ve User ekleme ekranı yapıldı

--- GarajYeri.Web/Controllers/UserController.cs
+++ GarajYeri.Web/Controllers/UserController.cs
@@ -21,20 +21,61 @@ namespace GarajYeri.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(AppUser appUser)
         {
-            AppUser user = _context.Users.FirstOrDefault(u=>u.UserName == appUser.UserName && u.Password == appUser.Password);
-            if (user != null) {
-             List<Claim> claims = new List<Claim>();
+            AppUser user = _context.Users.FirstOrDefault(u => u.UserName == appUser.UserName && u.Password == appUser.Password);
+            if (user != null)
+            {
+                List<Claim> claims = new List<Claim>();
                 claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
                 claims.Add(new Claim(ClaimTypes.Name, user.UserName));
                 claims.Add(new Claim(ClaimTypes.GivenName, user.FullName));
                 claims.Add(new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User"));
-                ClaimsIdentity identity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
-                 await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-                 await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
                 return RedirectToAction("Index", "Vehicle");
             }
             return View();
 
         }
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _context.Users.Where(u=>!u.IsDeleted).ToList() });
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(AppUser appUser)
+        {
+            _context.Users.Add(appUser);
+            _context.SaveChanges();
+            return Ok(appUser);
+        }
+        [HttpPost]
+        public IActionResult HardDelete(AppUser appUser)
+        {
+            _context.Users.Remove(appUser);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult SoftDelete(int id)
+        {
+            AppUser appUser = _context.Users.Find(id);
+            appUser.IsDeleted = true;
+            _context.Users.Update(appUser);
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Update(AppUser appUser) {
+            _context.Users.Update(appUser);
+            _context.SaveChanges();
+            return Ok(appUser);
+        }
+
     }
 }
